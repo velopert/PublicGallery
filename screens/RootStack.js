c32fa70815adf7a5eefbbee3e4cea4f285fb5379
velopert <public.velopert@gmail.com>
2021-05-10 04:47:02 +0900

    9.2

--- screens/RootStack.js
+++ screens/RootStack.js
@@ -4,11 +4,32 @@ import SignInScreen from './SignInScreen';
 import WelcomeScreen from './WelcomeScreen';
 import {useUserContext} from '../contexts/UserContext';
 import MainTab from './MainTab';
+import {useEffect} from 'react';
+import {getUser} from '../lib/users';
+import {subscribeAuth} from '../lib/auth';
 
 const Stack = createStackNavigator();
 
 function RootStack() {
-  const {user} = useUserContext();
+  const {user, setUser} = useUserContext();
+
+  useEffect(() => {
+    // 컴포넌트 첫 로딩시 로그인 상태 확인하고 UserContext에 적용
+    const unsubscribe = subscribeAuth(async currentUser => {
+      // 여기에 등록한 함수는 사용자 정보 바뀔 때 마다 호출되는데,
+      // 처음 호출 될 떄 바로 unsubscribe하여 한번 호출 후에는 더 이상 호출되지 않게 설정
+      unsubscribe();
+      if (!currentUser) {
+        return;
+      }
+      const profile = await getUser(currentUser.uid);
+      if (!profile) {
+        return;
+      }
+      setUser(profile);
+    });
+  }, [setUser]);
+
   return (
     <Stack.Navigator>
       {user ? (

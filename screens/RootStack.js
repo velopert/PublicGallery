52c4a53941e4bfc0f1089eff80b3eb82ce1614ca
velopert <public.velopert@gmail.com>
2021-08-19 02:05:58 +0900

    8.1 Setup project

---
+++ screens/RootStack.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+function RootStack() {
+  return <Stack.Navigator>{/* 화면 추가 예정 */}</Stack.Navigator>;
+}
+
+export default RootStack;

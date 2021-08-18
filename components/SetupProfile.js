f534b735af29662355cad1c42b7f8c96dba25a90
velopert <public.velopert@gmail.com>
2021-08-19 04:03:08 +0900

    8.5 Upload Image

--- components/SetupProfile.js
+++ components/SetupProfile.js
@@ -1,6 +1,15 @@
 import {useNavigation, useRoute} from '@react-navigation/native';
 import React, {useState} from 'react';
-import {StyleSheet, View} from 'react-native';
+import {
+  Pressable,
+  StyleSheet,
+  View,
+  Platform,
+  Image,
+  ActivityIndicator,
+} from 'react-native';
+import {launchImageLibrary} from 'react-native-image-picker';
+import storage from '@react-native-firebase/storage';
 import {signOut} from '../lib/auth';
 import {createUser} from '../lib/users';
 import BorderedInput from './BorderedInput';
@@ -10,18 +19,42 @@ import {useUserContext} from '../contexts/UserContext';
 function SetupProfile() {
   const [displayName, setDisplayName] = useState('');
   const navigation = useNavigation();
+  const [response, setResponse] = useState(null);
 
   const {setUser} = useUserContext();
 
   const {params} = useRoute();
   const {uid} = params || {};
 
-  const onSubmit = () => {
+  const [loading, setLoading] = useState(false);
+
+  const onSubmit = async () => {
+    setLoading(true);
+
+    let photoURL = null;
+
+    if (response) {
+      const asset = response.assets[0];
+      const extension = asset.fileName.split('.').pop(); // 확장자 추출
+      const reference = storage().ref(`/profile/${uid}.${extension}`);
+
+      if (Platform.OS === 'android') {
+        await reference.putString(asset.base64, 'base64', {
+          contentType: asset.type,
+        });
+      } else {
+        await reference.putFile(asset.uri);
+      }
+
+      photoURL = response ? await reference.getDownloadURL() : null;
+    }
+
     const user = {
       id: uid,
       displayName,
-      photoURL: null,
+      photoURL,
     };
+
     createUser(user);
     setUser(user);
   };
@@ -31,9 +64,38 @@ function SetupProfile() {
     navigation.goBack();
   };
 
+  const onSelectImage = () => {
+    launchImageLibrary(
+      {
+        mediaType: 'photo',
+        maxWidth: 512,
+        maxHeight: 512,
+        includeBase64: Platform.OS === 'android',
+      },
+      res => {
+        if (res.didCancel) {
+          // 취소했을 경우
+          return;
+        }
+        console.log(res);
+        setResponse(res);
+      },
+    );
+  };
+
   return (
     <View style={styles.block}>
-      <View style={styles.circle} />
+      <Pressable onPress={onSelectImage}>
+        <Image
+          style={styles.circle}
+          source={
+            response
+              ? {uri: response?.assets[0]?.uri}
+              : require('../assets/user.png')
+          }
+        />
+      </Pressable>
+
       <View style={styles.form}>
         <BorderedInput
           placeholder="닉네임"
@@ -42,10 +104,14 @@ function SetupProfile() {
           onSubmitEditing={onSubmit}
           returnKeyType="next"
         />
-        <View style={styles.buttons}>
-          <CustomButton title="다음" onPress={onSubmit} hasMarginBottom />
-          <CustomButton title="취소" onPress={onCancel} theme="secondary" />
-        </View>
+        {loading ? (
+          <ActivityIndicator size={32} color="#6200ee" style={styles.spinner} />
+        ) : (
+          <View style={styles.buttons}>
+            <CustomButton title="다음" onPress={onSubmit} hasMarginBottom />
+            <CustomButton title="취소" onPress={onCancel} theme="secondary" />
+          </View>
+        )}
       </View>
     </View>
   );
@@ -70,6 +136,10 @@ const styles = StyleSheet.create({
   },
   buttons: {
     marginTop: 48,
+  },
+  spinner: {
+    marginTop: 48,
+    height: 104,
   },
 });
 
--- screens/MainTab.js
+++ screens/MainTab.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Image, StyleSheet, Text, View} from 'react-native';
 import {useUserContext} from '../contexts/UserContext';
 
 function MainTab() {
   const {user} = useUserContext();
   return (
     <View style={styles.block}>
+      {user.photoURL && (
+        <Image
+          source={{uri: user.photoURL}}
+          style={{width: 128, height: 128, marginBottom: 16}}
+          resizeMode="cover"
+        />
+      )}
       <Text style={styles.text}>Hello, {user.displayName}</Text>
     </View>
   );

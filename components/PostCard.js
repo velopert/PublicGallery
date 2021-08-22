494b8ae453c64f40cb1304c5e22efafd744b329d
velopert <public.velopert@gmail.com>
2021-08-22 16:06:31 +0900

    9.4 List posts

---
+++ components/PostCard.js
@@ -0,0 +1,95 @@
+import React, {useMemo} from 'react';
+import {View, StyleSheet, Text, Image, Pressable} from 'react-native';
+
+function PostCard({user, photoURL, description, createdAt, id}) {
+  const date = useMemo(
+    () => (createdAt ? new Date(createdAt._seconds * 1000) : new Date()),
+    [createdAt],
+  );
+
+  const onOpenProfile = () => {
+    // TODO: 사용자 프로필 화면 열기
+  };
+
+  return (
+    <View style={styles.block}>
+      <View style={[styles.head, styles.paddingBlock]}>
+        <Pressable style={styles.profile} onPress={onOpenProfile}>
+          <Image
+            source={
+              user.photoURL
+                ? {
+                    uri: user.photoURL,
+                  }
+                : require('../assets/user.png')
+            }
+            resizeMode="cover"
+            style={styles.avatar}
+          />
+          <Text style={styles.displayName}>{user.displayName}</Text>
+        </Pressable>
+      </View>
+      <Image
+        source={{uri: photoURL}}
+        style={styles.image}
+        resizeMethod="resize"
+        resizeMode="cover"
+      />
+      <View style={styles.paddingBlock}>
+        <Text style={styles.description}>{description}</Text>
+        <Text date={date} style={styles.date}>
+          {date.toLocaleString()}
+        </Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  block: {
+    paddingTop: 16,
+    paddingBottom: 16,
+  },
+  avatar: {
+    width: 32,
+    height: 32,
+    borderRadius: 16,
+  },
+  paddingBlock: {
+    paddingHorizontal: 16,
+  },
+  head: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 16,
+  },
+  profile: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  displayName: {
+    lineHeight: 16,
+    fontSize: 16,
+    marginLeft: 8,
+    fontWeight: 'bold',
+  },
+  image: {
+    backgroundColor: '#bdbdbd',
+    width: '100%',
+    aspectRatio: 1,
+    marginBottom: 16,
+  },
+  description: {
+    fontSize: 16,
+    lineHeight: 24,
+    marginBottom: 8,
+  },
+  date: {
+    color: '#757575',
+    fontSize: 12,
+    lineHeight: 18,
+  },
+});
+
+export default PostCard;
--- screens/FeedScreen.js
+++ screens/FeedScreen.js
@@ -1,8 +1,87 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  ActivityIndicator,
+  FlatList,
+  RefreshControl,
+  StyleSheet,
+} from 'react-native';
+import PostCard from '../components/PostCard';
+import {getNewerPosts, getOlderPosts, getPosts, PAGE_SIZE} from '../lib/posts';
 
 function FeedScreen() {
-  return <View />;
+  const [posts, setPosts] = useState(null);
+  // 마지막 포스트까지 조회했음을 명시하는 상태
+  const [noMorePost, setNoMorePost] = useState(false);
+  const [refreshing, setRefreshing] = useState(false);
+
+  useEffect(() => {
+    // 컴포넌트가 처음 마운트될 때 포스트 목록을 조회한 후 `posts` 상태에 담기
+    getPosts().then(setPosts);
+  }, []);
+
+  const onLoadMore = async () => {
+    if (noMorePost || !posts || posts.length < PAGE_SIZE) {
+      return;
+    }
+    const lastPost = posts[posts.length - 1];
+    const olderPosts = await getOlderPosts(lastPost.id);
+    if (olderPosts.length < PAGE_SIZE) {
+      setNoMorePost(true);
+    }
+    setPosts(posts.concat(olderPosts));
+  };
+
+  const onRefresh = async () => {
+    if (!posts || posts.length === 0 || refreshing) {
+      return;
+    }
+    const firstPost = posts[0];
+    setRefreshing(true);
+    const newerPosts = await getNewerPosts(firstPost.id);
+    setRefreshing(false);
+    if (newerPosts.length === 0) {
+      return;
+    }
+    setPosts(newerPosts.concat(posts));
+  };
+
+  return (
+    <FlatList
+      data={posts}
+      renderItem={renderItem}
+      keyExtractor={item => item.id}
+      contentContainerStyle={styles.container}
+      onEndReached={onLoadMore}
+      onEndReachedThreshold={0.75}
+      ListFooterComponent={
+        !noMorePost && (
+          <ActivityIndicator style={styles.spinner} size={32} color="#6200ee" />
+        )
+      }
+      refreshControl={
+        <RefreshControl onRefresh={onRefresh} refreshing={refreshing} />
+      }
+    />
+  );
 }
+
+const renderItem = ({item}) => (
+  <PostCard
+    createdAt={item.createdAt}
+    description={item.description}
+    id={item.id}
+    user={item.user}
+    photoURL={item.photoURL}
+  />
+);
+
+const styles = StyleSheet.create({
+  container: {
+    paddingBottom: 48,
+  },
+  spinner: {
+    height: 64,
+  },
+});
 
 export default FeedScreen;
